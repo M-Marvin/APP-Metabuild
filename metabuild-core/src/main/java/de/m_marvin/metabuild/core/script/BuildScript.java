13d476fdbc9dfcd7c9f8cc498243490c32570cf2
M-Marvin <69386758+M-Marvin@users.noreply.github.com>
2025-09-05 17:55:37 +0200

    Update to metabuild version 1.2 and add upgrade script
    Changed all metabuild project version declarations from '1.2-SNAPSHOT' to '1.2'. Updated .classpath files to reference released jars instead of snapshots. Added property import and substitution support to BuildScript. Added upgrade.cmd for batch initialization of all modules with current system version of the Buildsystem. Updated metabuild-wrapper.jar binaries.

--- metabuild-core/src/main/java/de/m_marvin/metabuild/core/script/BuildScript.java
+++ metabuild-core/src/main/java/de/m_marvin/metabuild/core/script/BuildScript.java
@@ -1,7 +1,14 @@
 package de.m_marvin.metabuild.core.script;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 import de.m_marvin.metabuild.core.Metabuild;
 import de.m_marvin.metabuild.core.exception.BuildScriptException;
@@ -20,7 +27,7 @@ public class BuildScript {
 	 * Filled out by the metabuild main class when importing the build file */
 	public String buildName = "";
 	public File buildfileLocation = new File("");
-
+	
 	protected void importBuild(File locationOrBuildFile) {
 		importBuild(null, locationOrBuildFile);
 	}
@@ -43,13 +50,45 @@ public class BuildScript {
 	}
 	
 	protected Object field(String fieldName) {
+		return field(this, fieldName);
+	}
+	
+	protected Object field(BuildScript buildscript, String fieldName) {
 		try {
-			Field f = this.getClass().getDeclaredField(fieldName);
+			Field f = buildscript.getClass().getDeclaredField(fieldName);
 			f.setAccessible(true);
-			return f.get(this);
+			return f.get(buildscript);
 		} catch (Exception e) {
 			throw BuildScriptException.msg("field '%s' is not availble in build '%s'", fieldName, this.buildName);
 		}
+	}
+
+	/**
+	 * Hold property mapping defined by this build script or imported from an properties file.
+	 */
+	public Map<String, String> properties = new HashMap<>();
+	
+	protected void importProperties(File location) {
+		try {
+			BufferedReader pin = new BufferedReader(new InputStreamReader(new FileInputStream(FileUtility.absolute(location))));
+			String line;
+			while ((line = pin.readLine()) != null) {
+				int i = line.indexOf('=');
+				if (i == -1) continue;
+				String key = line.substring(0, i);
+				String value = line.substring(i + 1);
+				this.properties.put(key.strip(), value.strip());
+			}
+			pin.close();
+		} catch (IOException e) {
+			throw BuildScriptException.msg("could not import properties file '%s' in build '%s'", location, this.buildName);
+		}
+	}
+	
+	private static final Pattern PROP_KEY = Pattern.compile("\\$\\{([^\\s${}]+)}");
+	
+	protected String fillProperties(String str) {
+		return PROP_KEY.matcher(str).replaceAll(m -> this.properties.getOrDefault(m.group(1), m.group(1)));
 	}
 	
 	/**
---
+++ upgrade.cmd
@@ -0,0 +1,10 @@
+cd metabuild-core
+cmd /C meta init -force -info
+cd ../metabuild-cpp
+cmd /C meta init -force -info
+cd ../metabuild-java
+cmd /C meta init -force -info
+cd ../metabuild-maven
+cmd /C meta init -force -info
+cd ../metabuild-wrapper
+cmd /C meta init -force -info
